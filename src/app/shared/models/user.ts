902aaecf83248717adcbc6f22fb3ebfc8235a512
evagrandet <evagrandet@ro.ru>
2019-11-23 18:14:40 +0300

    replaces class instead inteface (and i dont like it)

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,6 +1,8 @@
-export interface User {
-    email: string;
-    password: string;
-    name: string;
-    id?: number;
+export class User {
+    constructor(
+        public email: string,
+        public password: string,
+        public name: string,
+        public id?: number,
+    ) {}
 }
