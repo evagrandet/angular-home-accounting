7c58c825169d859b9602da17702f230079a125d9
evagrandet <evagrandet@ro.ru>
2020-01-05 19:09:16 +0300

    gets data into component

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BillService } from '../shared/services/bill.service';
+import { Bill } from '../shared/models/bill';
+import { combineLatest, Subscription, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-bill-page',
   templateUrl: './bill-page.component.html',
   styleUrls: ['./bill-page.component.scss']
 })
-export class BillPageComponent implements OnInit {
+export class BillPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    subscription: Subscription;
+
+  constructor(private billService: BillService) { }
 
   ngOnInit() {
+      this.subscription = combineLatest(
+          this.billService.getBill(),
+          this.billService.getCurrency())
+          .subscribe(
+              (data: [Bill, any]) => console.log(data));
+  }
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
 
 }
