1dc2e3646cff5bd8d9b08cb551f18ac8968855dc
evagrandet <evagrandet@ro.ru>
2020-01-11 14:37:11 +0300

    work in progress

--- src/app/system/bill-page/currency-card/currency-card.component.ts
+++ src/app/system/bill-page/currency-card/currency-card.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input,  } from '@angular/core';
+import { Component, Input, OnInit,  } from '@angular/core';
 
 @Component({
   selector: 'app-currency-card',
   templateUrl: './currency-card.component.html',
   styleUrls: ['./currency-card.component.scss']
 })
-export class CurrencyCardComponent {
+export class CurrencyCardComponent implements OnInit {
     @Input() currency: any;
-    constructor() { }
-
+    constructor() {
+    }
+    ngOnInit() {
+        console.log(this.currency);
+    }
 }
