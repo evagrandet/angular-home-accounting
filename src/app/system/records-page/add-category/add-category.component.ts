e8ffe7722baf9fddf7e252685ee99768cf60cbb3
evagrandet <evagrandet@ro.ru>
2020-02-19 07:32:56 +0300

    adds add-category component

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { Category } from 'src/app/shared/models/category';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+    category: Category;
+    @Output() categoryAdd = new EventEmitter<Category>();
 
-  constructor() { }
+    constructor(private categoryService: CategoriesService) { }
 
-  ngOnInit() {
-  }
+    onSubmit(form: NgForm) {
+        this.categoryService.addCategory(form.value)
+        .subscribe((value) => {
+            form.reset();
+            this.categoryAdd.emit(this.category);
+        });
 
+    }
 }
