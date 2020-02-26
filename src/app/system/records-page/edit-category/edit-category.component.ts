99c53e711b1fbdedeb80e51bd368bad67216335b
evagrandet <evagrandet@ro.ru>
2020-02-26 20:12:41 +0300

    adds edit category

--- src/app/system/records-page/edit-category/edit-category.component.ts
+++ src/app/system/records-page/edit-category/edit-category.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Category } from 'src/app/shared/models/category';
 
 @Component({
-  selector: 'app-edit-category',
-  templateUrl: './edit-category.component.html',
-  styleUrls: ['./edit-category.component.scss']
+    selector: 'app-edit-category',
+    templateUrl: './edit-category.component.html',
+    styleUrls: ['./edit-category.component.scss'],
 })
 export class EditCategoryComponent implements OnInit {
+    @Input() categories: Category[];
+    @Output() categoryEdit = new EventEmitter<Category>();
+    currentCategoryId = 1;
+    currentCategory: Category;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.onCategoryChanged();
+    }
 
+    onSubmit(form: NgForm) {
+        let { capacity, name } = form.value;
+        if (capacity < 0) {
+            capacity *= -1;
+        }
+    }
+
+    onCategoryChanged() {
+        this.currentCategory = this.categories.find(c => c.id == this.currentCategoryId);
+    }
+
+    updateCa;
 }
