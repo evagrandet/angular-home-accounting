277e999a452062b8247261d0228a1ce3859c8136
evagrandet <evagrandet@ro.ru>
2020-02-26 20:13:45 +0300

    adds method

---
+++ src/app/system/shared/services/categories.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from 'app/shared/models/category';
+import { BaseApi } from 'app/shared/core/base-api';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CategoriesService extends BaseApi {
+    constructor(public http: HttpClient) {
+        super(http);
+    }
+    addCategory(category: Category): Observable<Category> {
+        return this.post('categories', category);
+    }
+
+    getCategories(): Observable<Category[]> {
+        return this.get('categories');
+    }
+}
