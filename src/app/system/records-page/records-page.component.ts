accabbb7953621da091445ffd3e9cf7e514aa381
evagrandet <evagrandet@ro.ru>
2020-03-02 05:20:37 +0300

    integrates records page

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'app/shared/models/category';
+import { CategoriesService } from '../../shared/services/categories.service';
 
 @Component({
-  selector: 'app-records-page',
-  templateUrl: './records-page.component.html',
-  styleUrls: ['./records-page.component.scss']
+    selector: 'app-records-page',
+    templateUrl: './records-page.component.html',
+    styleUrls: ['./records-page.component.scss'],
 })
 export class RecordsPageComponent implements OnInit {
+    categories: Category[] = [];
+    isLoad = false;
 
-  constructor() { }
+    constructor(private categoriesService: CategoriesService) {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.categoriesService.getCategories().subscribe((categories: Category[]) => {
+            this.categories = categories;
+            this.isLoad = true;
+        });
+    }
+    newCategoryAdded(category: Category) {
+        this.categories.push(category);
+    }
+    categoryEditted(category: Category) {
+        const index = this.categories.findIndex(item => item.id == category.id);
+        this.categories[index] = category;
+    }
 }
