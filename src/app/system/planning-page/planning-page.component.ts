9870356f5bf5c6b8bdf37fbb537bc1b5d70c486c
evagrandet <evagrandet@ro.ru>
2020-03-03 05:25:33 +0300

    adds planning page

--- src/app/system/planning-page/planning-page.component.ts
+++ src/app/system/planning-page/planning-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { BillService } from 'app/shared/services/bill.service';
+import { CategoriesService } from 'app/shared/services/categories.service';
+import { ActionsService } from 'app/shared/services/actions.service';
 
 @Component({
-  selector: 'app-planning-page',
-  templateUrl: './planning-page.component.html',
-  styleUrls: ['./planning-page.component.scss']
+    selector: 'app-planning-page',
+    templateUrl: './planning-page.component.html',
+    styleUrls: ['./planning-page.component.scss'],
 })
 export class PlanningPageComponent implements OnInit {
+    constructor(
+        private billService: BillService,
+        private categoriesService: CategoriesService,
+        private actionService: ActionsService,
+    ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
