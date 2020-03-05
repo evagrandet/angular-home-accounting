9d0a6ebd966611bdd1e4f25e0ead5dcd1e012101
evagrandet <evagrandet@ro.ru>
2020-03-05 07:07:20 +0300

    imports new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 import { SystemModule } from './system/system.module';
 import { SystemRoutingModule } from './system/system-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    AuthModule,
-    SharedModule,
-    SystemModule,
-    SystemRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AuthModule,
+        SharedModule,
+        SystemModule,
+        SystemRoutingModule,
+        NgxChartsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -18,6 +18,10 @@ import { AddCategoryComponent } from './records-page/add-category/add-category.c
 import { EditCategoryComponent } from './records-page/edit-category/edit-category.component';
 import { CategoriesService } from '../shared/services/categories.service';
 import { ActionsService } from 'app/shared/services/actions.service';
+import { HistoryChartComponent } from './history-page/history-chart/history-chart.component';
+import { HistoryEventsComponent } from './history-page/history-events/history-events.component';
+import { HistoryDetailComponent } from './history-page/history-detail/history-detail.component';
+import { HistoryFilterComponent } from './history-page/history-filter/history-filter.component';
 
 @NgModule({
     declarations: [
@@ -33,6 +37,10 @@ import { ActionsService } from 'app/shared/services/actions.service';
         AddEventComponent,
         AddCategoryComponent,
         EditCategoryComponent,
+        HistoryChartComponent,
+        HistoryEventsComponent,
+        HistoryDetailComponent,
+        HistoryFilterComponent,
     ],
     imports: [CommonModule, SharedModule, SystemRoutingModule],
     providers: [BillService, CategoriesService, ActionsService],
