5f38c2ef97972160847d94b799acdf7ac5d8ae49
evagrandet <evagrandet@ro.ru>
2020-03-11 19:49:11 +0300

    adds chart's component to history page

--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'app/shared/services/categories.service';
+import { ActionsService } from 'app/shared/services/actions.service';
+import { Subscription, combineLatest } from 'rxjs';
+import { Category } from 'app/shared/models/category';
+import { Action } from 'app/shared/models/action';
 
 @Component({
-  selector: 'app-history-page',
-  templateUrl: './history-page.component.html',
-  styleUrls: ['./history-page.component.scss']
+    selector: 'app-history-page',
+    templateUrl: './history-page.component.html',
+    styleUrls: ['./history-page.component.scss'],
 })
 export class HistoryPageComponent implements OnInit {
+    isLoad = false;
+    stream$: Subscription;
+    categories: Category[];
+    actions: Action[];
+    chartData: [];
 
-  constructor() { }
+    constructor(private categoriesService: CategoriesService, private actionsService: ActionsService) {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.stream$ = combineLatest(
+            this.categoriesService.getCategories(),
+            this.actionsService.getActions(),
+        ).subscribe((data: [Category[], Action[]]) => {
+            this.categories = data[0];
+            this.actions = data[1];
+            this.calculateChartData();
+            this.isLoad = true;
+        });
+    }
+    calculateChartData(): void {
+        this.chartData = [];
+        this.categories.forEach((category: Category) => {
+            console.log(category);
+            let categoryEvents = this.actions.filter(
+                (action: Action) => action.category === category.id && action.type === 'outcome',
+            );
+            this.chartData.push({
+                name: category.name,
+                value: categoryEvents.reduce((total, event) => {
+                    total += event.amount;
+                    return total;
+                }, 0),
+            });
+            console.log(this.chartData);
+        });
+    }
+    ngOnDestroy(): void {
+        if (this.stream$) {
+            this.stream$.unsubscribe();
+        }
+    }
 }
