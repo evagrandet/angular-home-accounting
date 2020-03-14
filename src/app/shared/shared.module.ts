d58490dbdc02e42fc1ecdc0d4c53dfc8cd2cc292
evagrandet <evagrandet@ro.ru>
2020-03-14 22:23:42 +0300

    adds and integrates history events component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-    declarations: [],
+    declarations: [FilterPipe],
     imports: [FormsModule, ReactiveFormsModule, NgxChartsModule],
-    exports: [FormsModule, ReactiveFormsModule, NgxChartsModule],
+    exports: [FormsModule, ReactiveFormsModule, NgxChartsModule, FilterPipe],
 })
 export class SharedModule {}
--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Category } from 'app/shared/models/category';
+import { Action } from 'app/shared/models/action';
 
 @Component({
-  selector: 'app-history-events',
-  templateUrl: './history-events.component.html',
-  styleUrls: ['./history-events.component.scss']
+    selector: 'app-history-events',
+    templateUrl: './history-events.component.html',
+    styleUrls: ['./history-events.component.scss'],
 })
 export class HistoryEventsComponent implements OnInit {
+    @Input() categories: Category[];
+    @Input() actions: Action[];
+    searchValue = '';
+    searchPlaceholder = 'Сумма';
+    searchField = 'amount';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        console.log(this.actions);
+        this.actions.forEach((action: Action) => {
+            action.categoryName = this.categories.find((category: Category) => category.id === action.category)['name'];
+        });
+    }
+    selectOption(option: string) {
+        const namesMap = {
+            amount: 'Сумма',
+            date: 'Дата',
+            category: 'Категория',
+            type: 'Тип',
+        };
+        this.searchPlaceholder = namesMap[option];
+        this.searchField = option;
+    }
 }
