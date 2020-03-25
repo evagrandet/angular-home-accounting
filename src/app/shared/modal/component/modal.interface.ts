cbdfc5c5bfce12241483651b06a5bff09ac05f7e
evagrandet <evagrandet@ro.ru>
2020-03-25 22:43:58 +0300

    adds modal's interface

---
+++ src/app/shared/modal/component/modal.interface.ts
@@ -0,0 +1,11 @@
+export interface ModalComponentInterface {
+    /**
+     * Dismiss modal
+     */
+    dismiss(reason?: any): any;
+
+    /**
+     * Close modal
+     */
+    close(value?: any): any;
+}
