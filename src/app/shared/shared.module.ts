ced27f7254ffccc4e877639b68b714928fea5a4a
evagrandet <evagrandet@ro.ru>
2020-04-02 22:12:26 +0300

    imports 404 rage and load component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { FilterPipe } from './pipes/filter.pipe';
+import { Error404Component } from './components/error404/error404.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-    declarations: [FilterPipe],
+    declarations: [FilterPipe, Error404Component, LoaderComponent],
     imports: [FormsModule, ReactiveFormsModule, NgxChartsModule],
-    exports: [FormsModule, ReactiveFormsModule, NgxChartsModule, FilterPipe],
+    exports: [FormsModule, ReactiveFormsModule, NgxChartsModule, FilterPipe, Error404Component, LoaderComponent],
 })
 export class SharedModule {}
