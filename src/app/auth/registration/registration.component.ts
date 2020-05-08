29d4a67fc0b08640333ce0baafe476766abeb744
evagrandet <evagrandet@ro.ru>
2020-05-08 23:30:44 +0300

    deletes console.log

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -40,7 +40,6 @@ export class RegistrationComponent implements OnInit {
     forbiddenEmails(control: FormControl): Promise<any> {
         return new Promise((resolve, reject) => {
             this.usersService.getUserByEmail(control.value).subscribe((user: User) => {
-                console.log(user);
                 if (user) {
                     resolve({ forbiddenEmail: true });
                 } else {
